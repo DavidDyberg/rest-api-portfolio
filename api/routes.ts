cd9b8ec4370fc1339d0f0ba020e9a201b14f6032
David Dyberg <david.dyberg@gmail.com>
2025-03-11 20:36:36 +0100

    feat: add crud functionallity for projects

--- api/routes.ts
+++ api/routes.ts
@@ -1,7 +1,20 @@
 import { Router } from "express";
 import { getUser, updateUser } from "../controllers/userController";
+import {
+  createProject,
+  deleteProject,
+  getAllProjects,
+  getProjectById,
+  updateProject,
+} from "../controllers/projectsController";
 
 export const router = Router();
 
 router.get("/about", getUser);
 router.put("/about", updateUser);
+
+router.get("/projects", getAllProjects);
+router.get("/projects/:id", getProjectById);
+router.post("/projects", createProject);
+router.put("/projects/:id", updateProject);
+router.delete("/projects/:id", deleteProject);
---
+++ controllers/projectsController.ts
@@ -0,0 +1,77 @@
+import { request, Request, response, Response } from "express";
+
+let mockProjects = [
+  {
+    id: 1,
+    title: "IMDB Clone",
+    description: "A website for managing your favorite movies",
+    techStack: ["PHP", "Laravel", "MariaDB"],
+    githubLink: "https://github.com/example",
+    liveDemo: "example.vercel.app",
+    image: "",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 2,
+    title: "Todo list",
+    description: "A website for managing your todos",
+    techStack: ["PHP", "MariaDB"],
+    githubLink: "https://github.com/example",
+    liveDemo: "example.vercel.app",
+    image: "",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+export const getAllProjects = (req: Request, res: Response) => {
+  res.json(mockProjects);
+};
+
+export const getProjectById = (req: Request, res: Response) => {
+  const project = mockProjects.find(
+    (project) => project.id === parseInt(req.params.id)
+  );
+  res.json(project);
+};
+
+export const createProject = (req: Request, res: Response) => {
+  const newProject = {
+    id: mockProjects.length + 1,
+    ...req.body,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+  mockProjects.push(newProject);
+  res.status(201).json({ message: "Project created successfully" });
+};
+
+export const updateProject = (req: Request, res: Response) => {
+  const projectId = parseInt(req.params.id);
+  const projectIndex = mockProjects.findIndex(
+    (project) => project.id === projectId
+  );
+
+  if (projectIndex === -1) {
+    res.status(404).json({ message: "Project not found" });
+  }
+
+  const updatedProject = {
+    ...mockProjects[projectIndex],
+    ...req.body,
+    updatedAt: new Date(),
+  };
+
+  mockProjects[projectIndex] = updatedProject;
+  res
+    .status(200)
+    .json({ message: "Project updated successfully", project: updatedProject });
+};
+
+export const deleteProject = (req: Request, res: Response) => {
+  mockProjects = mockProjects.filter(
+    (project) => project.id != parseInt(req.params.id)
+  );
+  res.json({ message: "Project was deleted successfully " });
+};
