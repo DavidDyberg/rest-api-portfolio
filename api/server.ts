8018faa12c55685f714091d9762b1d715c37c2e5
David Dyberg <david.dyberg@gmail.com>
2025-03-11 12:20:28 +0100

    feat: setup database connection

--- api/server.ts
+++ api/server.ts
@@ -1,7 +1,12 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import connectDB from "../database/db";
+
+dotenv.config();
+connectDB();
 
 const app: Express = express();
-const PORT: string | number = 4000;
+const PORT: string | number = process.env.PORT || 4000;
 
 app.use(express.json());
 
---
+++ database/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+async function connectDB() {
+  try {
+    const URL = process.env.MONGO_DB_URL || "";
+    await mongoose.connect(URL);
+    console.log("Database connected successfully");
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(`Error while connecting to database: ${error.message}`);
+    }
+  }
+}
+
+export default connectDB;
