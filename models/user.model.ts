88b1756f1bb897a751b07d629d8a0c4bbdec3e79
David Dyberg <david.dyberg@gmail.com>
2025-03-17 13:35:44 +0100

    feat: add timestamps for user model

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,17 +1,20 @@
 import mongoose, { InferSchemaType } from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  age: { type: Number },
-  bio: { type: String },
-  profileImage: { type: String },
-  skills: { type: [String] },
-  socials: { type: Object },
-  phoneNumber: { type: String },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    age: { type: Number },
+    bio: { type: String },
+    profileImage: { type: String },
+    skills: { type: [String] },
+    socials: { type: Object },
+    phoneNumber: { type: String },
+  },
+  { timestamps: true }
+);
 
 export const User = mongoose.model<InferSchemaType<typeof UserSchema>>(
   "User",
