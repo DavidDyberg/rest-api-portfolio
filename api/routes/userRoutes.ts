8246d61c8669ee46a95ae8bdca81438d77e2b17a
David Dyberg <david.dyberg@gmail.com>
2025-03-24 12:42:48 +0100

    feat: add authMiddleware to user routes

--- api/routes/userRoutes.ts
+++ api/routes/userRoutes.ts
@@ -4,9 +4,10 @@ import {
   getUser,
   updateUser,
 } from "../../controllers/userController";
+import { authMiddleware } from "../../middleware/authMiddleware";
 
 export const userRouter = Router();
 
 userRouter.get("/about", getUser);
-userRouter.post("/about", createUser);
-userRouter.put("/about/:id", updateUser);
+userRouter.post("/about", authMiddleware, createUser);
+userRouter.put("/about/:id", authMiddleware, updateUser);
